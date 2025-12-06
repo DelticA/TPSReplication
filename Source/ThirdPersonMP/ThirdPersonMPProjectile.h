2e420407eb43b59f59e43b3745a0ddcee0aa5e9c
Delta <1615801681@qq.com>
2025-12-06 16:54:55 +0800

    投射物与复制

---
+++ Source/ThirdPersonMP/ThirdPersonMPProjectile.h
@@ -0,0 +1,59 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ThirdPersonMPProjectile.generated.h"
+
+UCLASS()
+class THIRDPERSONMP_API AThirdPersonMPProjectile : public AActor
+{
+	GENERATED_BODY()
+public:
+	/**
+	 * 这些声明中的各个类型前面都冠以 class 关键字。
+	 * 这样，这些声明除了是变量声明之外，还是各自类的前向声明，从而确保各自的类会在头文件中被识别。
+	 */
+	
+	// 用于测试碰撞的球体组件。
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	class USphereComponent* SphereComponent;
+
+	// 用于提供对象视觉呈现效果的静态网格体。
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	class UStaticMeshComponent* StaticMesh;
+
+	// 用于处理投射物移动的移动组件。
+	/**
+	 * 像角色移动组件一样，投射物移动组件在移动其所归属的Actor时，
+	 * 若该Actor的 bReplicates 设为 True，则投射物移动组件自动处理复制。
+	 */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components")
+	class UProjectileMovementComponent* ProjectileMovementComponent;
+
+	// 在投射物撞击其他对象并爆炸时使用的粒子。
+	UPROPERTY(EditAnywhere, Category = "Effects")
+	class UParticleSystem* ExplosionEffect;
+
+	//此投射物将造成的伤害类型和伤害。
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Damage")
+	TSubclassOf<class UDamageType> DamageType;
+
+	//此投射物造成的伤害。
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Damage")
+	float Damage;
+	
+public:	
+	// Sets default values for this actor's properties
+	AThirdPersonMPProjectile();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
